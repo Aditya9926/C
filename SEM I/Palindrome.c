b06a62a9a7e77da05c65298d1b0b1f7f77690ac9
Aditya Sahu <500096455@stu.upes.ac.in>
2025-10-09 22:15:12 +0530

    initial  commit

---
+++ SEM I/Palindrome.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, r, sum = 0, temp;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    temp = n;    
+    while (n != 0)
+    {
+        r = n % 10;
+        sum = sum * 10 + r;
+        n /= 10;
+    }
+    if (temp == sum)
+        printf("%d is a palindrome.\n", temp);
+    else
+        printf("%d is not a palindrome.\n", temp);
+    return 0;
+}
