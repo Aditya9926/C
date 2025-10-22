18b61b2e734fa03987657f88f4ebe0708e0c23f8
Aditya Sahu <500096455@stu.upes.ac.in>
2025-10-22 20:23:58 +0530

    initial commit

---
+++ SEM I/23_sizeof.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    printf("Size of int is : %zu",sizeof(int));
+    printf("\nSize of char is : %zu",sizeof(char));
+    printf("\nSize of float is : %zu",sizeof(float));
+    printf("\nSize of double is : %zu",sizeof(double));
+    printf("\nSize of void is : %zu",sizeof(void));
+}
