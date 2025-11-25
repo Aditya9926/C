b7ac2621d727a050c977875b755794f5fa1115e1
Aditya Sahu <500096455@stu.upes.ac.in>
2025-11-25 23:23:27 +0530

    initial commit

---
+++ SEM I/76_Perfect_num.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+    int n, i;
+    printf("Enter the upper limit (n): ");
+    scanf("%d", &n);
+
+    printf("Perfect numbers between 1 and %d are: \n", n);
+
+    for (i = 1; i <= n; i++) {
+        int sum = 0;
+        for (int j = 1; j <= i / 2; j++) {
+            if (i % j == 0) {
+                sum += j;
+            }
+        }
+        if (sum == i) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    return 0;
+}
